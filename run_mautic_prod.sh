7ef80355a8cf9efe0c729874a7f7d5bd2bcdcb7f
jdMac <akhtar1094@gmail.com>
2024-07-01 18:08:24 +0530

    added bash script for running run_mautic_prod

---
+++ run_mautic_prod.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the source file
+SOURCE_FILE="../conf/mautic_env.txt"
+
+# Define the destination symlink in the current directory
+DESTINATION_FILE="./.env"
+
+# Check if the source file exists
+if [ ! -f "$SOURCE_FILE" ]; then
+  echo "Source file $SOURCE_FILE does not exist."
+  exit 1
+fi
+
+# Copy the file, replacing it if it already exists
+cp -f "$SOURCE_FILE" "$DESTINATION_FILE"
+
+# Check if the copy operation was successful
+if [ $? -eq 0 ]; then
+  echo "File copied successfully to $DESTINATION_FILE"
+else
+  echo "Failed to copy the file."
+fi
