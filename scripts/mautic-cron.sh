6e8f3e448f08c114a64aa25e1b87678adf155575
jdMac <akhtar1094@gmail.com>
2024-09-16 14:54:53 +0530

    script updated to send emails every 30 minutes

--- scripts/mautic-cron.sh
+++ scripts/mautic-cron.sh
@@ -3,13 +3,13 @@
 # Get the current minute
 minute=$(date +%M)
 
-# If the minute is divisible by 5, run the first cron job
-if (( minute % 5 == 0 )); then
+# Run the first cron job if the minute is divisible by 30
+if (( minute % 30 == 0 )); then
+    /var/scripts/change-config.sh && \
+    php /var/www/html/bin/console mautic:campaigns:trigger --batch-limit=1 > /var/log/cron.pipe 2>&1
+# Otherwise, run the second cron job if the minute is divisible by 5
+elif (( minute % 5 == 0 )); then
     php /var/www/html/bin/console mautic:import --limit=500 && \
     php /var/www/html/bin/console mautic:segments:update --batch-limit=900 && \
     php /var/www/html/bin/console mautic:campaigns:rebuild --batch-limit=100 > /var/log/cron.pipe 2>&1
-else
-    # Otherwise, run the second cron job
-    /var/scripts/change-config.sh && \
-    php /var/www/html/bin/console mautic:campaigns:trigger --batch-limit=1 > /var/log/cron.pipe 2>&1
 fi
