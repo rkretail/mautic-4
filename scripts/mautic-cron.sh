00c1f6f1dcedfc5d00f83962e49c8c8a826af4ce
jdMac <akhtar1094@gmail.com>
2024-09-17 11:56:56 +0530

    changed mautic cron to send segment emails as campaigns emails not working for one email at a time

--- scripts/mautic-cron.sh
+++ scripts/mautic-cron.sh
@@ -6,7 +6,8 @@ minute=$(date +%M)
 # Run the first cron job if the minute is divisible by 30
 if (( minute % 30 == 0 )); then
     /var/scripts/change-config.sh && \
-    php /var/www/html/bin/console mautic:campaigns:trigger --batch-limit=1 > /var/log/cron.pipe 2>&1
+    php /var/www/html/bin/console mautic:broadcasts:send -l 1 > /var/log/cron.pipe 2>&1
+    # php /var/www/html/bin/console mautic:campaigns:trigger --batch-limit=1 > /var/log/cron.pipe 2>&1
 # Otherwise, run the second cron job if the minute is divisible by 5
 elif (( minute % 5 == 0 )); then
     php /var/www/html/bin/console mautic:import --limit=500 && \
