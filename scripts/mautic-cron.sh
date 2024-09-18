7bb529b0e803edeb82dbf7cf9dcb47c6d1b63938
jdMac <akhtar1094@gmail.com>
2024-09-18 13:01:46 +0530

    send email every 6 hours

--- scripts/mautic-cron.sh
+++ scripts/mautic-cron.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Get the current minute
+# Get the current hour and minute
+hour=$(date +%H)
 minute=$(date +%M)
 
-# Run the first cron job if the minute is divisible by 30
-if (( minute % 30 == 0 )); then
+# Run the first cron job if the hour is divisible by 6 and minute is 0
+if (( hour % 6 == 0 && minute == 0)); then
     /var/scripts/change-config.sh && \
     php /var/www/html/bin/console mautic:broadcasts:send -l 1 > /var/log/cron.pipe 2>&1
     # php /var/www/html/bin/console mautic:campaigns:trigger --batch-limit=1 > /var/log/cron.pipe 2>&1
